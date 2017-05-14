2a6fbd1ad1c3a4e474e06c4ec1c6d339c0fe56a0
theo <theo@ubuntu16>
2017-05-14 23:46:18 +0200

    Import ttn from API added

--- peil/peil/management/commands/ttn.py
+++ peil/peil/management/commands/ttn.py
@@ -7,18 +7,21 @@ from django.core.management.base import BaseCommand
 import json
 from peil.models import MasterModule, ECModule, PressureModule, GNSSModule,\
     Device, BaseModule
+from peil.models import GNSS_MESSAGE, STATUS_MESSAGE, EC_MESSAGE, PRESSURE_MESSAGE
 from django.db.utils import IntegrityError
 from django.db import transaction
 from django.utils.dateparse import parse_datetime
+from django.conf import settings
+import requests
 
 def update_or_create(manager, **kwargs):
     assert kwargs, \
             'update_or_create() must be passed at least one keyword argument'
     obj, created = manager.get_or_create(**kwargs)
-    defaults = kwargs.pop('defaults', {})
     if created:
         return obj, True, False
     else:
+        defaults = kwargs.pop('defaults', {})
         try:
             params = dict([(k, v) for k, v in kwargs.items() if '__' not in k])
             params.update(defaults)
@@ -36,86 +39,86 @@ def update_or_create(manager, **kwargs):
             except:
                 raise e
 
+def download_ttn(devid,since):
+    """ download data from The Things Network """
+    url = settings.TTN_URL
+    if devid:
+        url += '/' + devid
+    if since:
+        params = {'last': since }
+    else:
+        params = None
+    headers = {'Authorization': 'key '+settings.TTN_KEY, 'Accept': 'application/json'}
+    response = requests.get(url,params=params,headers=headers)
+    return response
+    
 class Command(BaseCommand):
-    args = ''
-    help = 'Import ttn file'
+    help = 'Download from The Things Network'
     
     def add_arguments(self, parser):
-        parser.add_argument('-f','--file',
-                action='store',
-                dest='fname',
-                help='ttn filename')
+        parser.add_argument('devid', type=str)
 
-        parser.add_argument('-s','--skip',
-                action='store_true',
-                default = False,
-                dest='skip',
-                help='Skip existing entries')
+        parser.add_argument('-s','--since',
+                action='store',
+                default = '1h',
+                dest='since',
+                help='Download since')
 
     def handle(self, *args, **options):
-        last = BaseModule.objects.all().order_by('time').last()
-        print 'Last message at {:%Y-%m-%d %H:%M:%S}'.format(last.time)
-        fname = options.get('fname')
-        skip = options.get('skip')
-        with open(fname) as f:
-            for line in f:
-                try:
-                    ttn = json.loads(line)
-                    pf = ttn['payload_fields']
-                except Exception as e:
-                    print line[:40], e
-                    continue
+        devid = options.get('devid')
+        since = options.get('since','1h')
+        device = Device.objects.get(devid=devid)
+        last = device.last()
+        print 'Last message at {:%Y-%m-%d %H:%M:%S}'.format(last)
+        response = download_ttn(devid, since)
+        if not response.ok:
+            print response.reason
+            return
+        ttns = response.json()
+        row = 0
+        for ttn in ttns:
+            row += 1
+            try:
+                time = parse_datetime(ttn['time'])
 
-                serial = ttn['hardware_serial']
-                devid = ttn['dev_id']
-                device, created = Device.objects.get_or_create(serial=serial,devid=devid)
-                
-                appid = ttn['app_id']
-                type = pf['type']
-                meta = ttn['metadata']
-                time = parse_datetime(meta['time'])
-                
-                if time < last.time and skip:
-                    #print devid, type, time, 'skipped' 
-                    #continue
-                    pass
+                print devid, time, 
+    
+                type = ttn['type']
 
-                print devid, type, time, 
-
-                if type == 6:
+                if type == STATUS_MESSAGE:
                     mod,created,updated = update_or_create(MasterModule.objects,device=device,time=time,type=type,defaults = {
-                        'appid': appid,
-                        'angle': pf['angle'],
-                        'battery': pf['battery'],
-                        'air': pf['pressure'],
-                        'total': pf['total']})
+                        'angle': ttn['angle'],
+                        'battery': ttn['battery'],
+                        'air': ttn['pressure'],
+                        'total': ttn['total']})
                     
-                elif type == 1:
+                elif type == GNSS_MESSAGE:
                     mod,created,updated = update_or_create(GNSSModule.objects,device=device,time=time,type=type, defaults = {
-                        'appid': appid,
-                        'gnsstime': pf['time'],
-                        'lat': pf['latitude'],
-                        'lon': pf['longitude'],
-                        'alt': pf['height'],
-                        'vacc': pf['vAcc'],
-                        'hacc': pf['hAcc'],
-                        'msl': pf['hMSL']})
+                        'gnsstime': ttn['time'],
+                        'lat': ttn['latitude'],
+                        'lon': ttn['longitude'],
+                        'alt': ttn['height'],
+                        'vacc': ttn['vAcc'],
+                        'hacc': ttn['hAcc'],
+                        'msl': ttn['hMSL']})
                     
-                elif type == 2:
+                elif type == EC_MESSAGE:
                     mod,created,updated = update_or_create(ECModule.objects,device=device,time=time,type=type, defaults = {
-                        'appid': appid,
-                        'position': pf['position'],
-                        'adc1': pf['ec1'],
-                        'adc2': pf['ec2'],
-                        'temperature': pf['temperature']})
-
-                elif type == 3:
+                        'position': ttn['position'],
+                        'adc1': ttn['ec1'],
+                        'adc2': ttn['ec2'],
+                        'temperature': ttn['temperature']})
+    
+                elif type == PRESSURE_MESSAGE:
                     mod,created,updated = update_or_create(PressureModule.objects,device=device,time=time,type=type, defaults = {
-                        'appid': appid,
-                        'position': pf['position'],
-                        'adc': pf['pressure']})
+                        'position': ttn['position'],
+                        'adc': ttn['pressure']})
                 
                 else:
                     raise Exception('Unknown module type:'+ str(type))
                 
                 print 'Created' if created else 'Updated' if updated else '?'
+            
+            except Exception as e:
+                print row, e
+                continue
