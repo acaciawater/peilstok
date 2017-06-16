aed91eb795ae2955d5343415a6d87fc0e07d05dc
Theo Kleinendorst <tkleinen@gmail.com>
2017-06-16 23:19:42 +0200

    map restyled

--- peil/peil/urls.py
+++ peil/peil/urls.py
@@ -40,3 +40,8 @@ urlpatterns = [
     url(r'^pop/(?P<pk>\d+)', PopupView.as_view()),
     url(r'^api/', include(v1.urls)),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
