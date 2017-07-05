ba5ae744e13c147a6ae5925f8d88fa6d9442caf0
Theo Kleinendorst <tkleinen@gmail.com>
2017-07-06 00:13:20 +0200

    transparent navbar, attributions removed for esri

--- peil/peil/static/js/mapstor.js
+++ peil/peil/static/js/mapstor.js
@@ -133,16 +133,16 @@ function initMap(div) {
 	});
 	
 	var topo = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+		attribution: 'Tiles &copy; Esri'
 	});
 	
 	var imagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+		attribution: 'Tiles &copy; Esri'
 	});
 
 	var waterlopen = L.tileLayer.wms('http://maps.acaciadata.com/geoserver/HHNK/wms', {
 		layers: 'HHNK:waterlopen_texel',
-		attribution: 'Hoogheemraadschap Hollands Noorderkwartier',
+		attribution: '&copy; <a href="https://www.hhnk.nl">hhnk.nl</a>',
 		format: 'image/png',
 		transparent: true});
 	
