05f3bf4ed923f2defa0c1f6fb9bd6843a32a6d42
Theo Kleinendorst <tkleinen@gmail.com>
2017-07-06 11:37:26 +0200

    allow all acaciadata.com sites

--- peil/peil/settings.py
+++ peil/peil/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['phzdmeet.nl','peil.acaciadata.com','localhost']
+ALLOWED_HOSTS = ['.phzdmeet.nl','.acaciadata.com','localhost']
 
 # Application definition
 
