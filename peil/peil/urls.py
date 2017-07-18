967b904521ef932c69ced90de1bddac522060dc9
Theo Kleinendorst <tkleinen@gmail.com>
2017-07-18 13:58:54 +0200

    changes admin page header

--- peil/peil/urls.py
+++ peil/peil/urls.py
@@ -48,3 +48,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = 'Beheerpagina'
