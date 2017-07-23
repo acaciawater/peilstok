d370e009a9bfc4591693b41b44446ccc6c4c9e7a
Theo Kleinendorst <tkleinen@gmail.com>
2017-07-24 00:47:02 +0200

    import ubx management command added

---
+++ peil/peil/management/commands/import_ubx.py
@@ -0,0 +1,22 @@
+'''
+Created on Apr 26, 2017
+
+@author: theo
+'''
+from django.core.management.base import BaseCommand
+from peil.util import add_ubx
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Import ubx file'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('-f','--file',
+                action='store',
+                dest='fname')
+
+    def handle(self, *args, **options):
+        fname = options.get('fname')
+        print fname
+        with open(fname) as f:
+            add_ubx(f)
