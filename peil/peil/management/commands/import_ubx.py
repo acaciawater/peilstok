90ff67a5719cbcd19e90088fa48be429000518fa
Theo Kleinendorst <tkleinen@gmail.com>
2017-08-11 22:40:29 +0200

    adds all ubxfiles from a folder

--- peil/peil/management/commands/import_ubx.py
+++ peil/peil/management/commands/import_ubx.py
@@ -5,6 +5,7 @@ Created on Apr 26, 2017
 '''
 from django.core.management.base import BaseCommand
 from peil.util import add_ubx
+import os
 
 class Command(BaseCommand):
     args = ''
@@ -14,9 +15,23 @@ class Command(BaseCommand):
         parser.add_argument('-f','--file',
                 action='store',
                 dest='fname')
+        parser.add_argument('-d','--dir',
+                action='store',
+                dest='dname')
 
     def handle(self, *args, **options):
         fname = options.get('fname')
-        print fname
-        with open(fname) as f:
-            add_ubx(f)
+        if fname:
+            print 'importing file', fname
+            with open(fname) as f:
+                add_ubx(f)
+                
+        dname = options.get('dname')
+        if dname:
+            print 'importing folder',dname
+            for path,_dirs,files in os.walk(dname):
+                for fname in files:
+                    print 'importing file', fname
+                    with open(os.path.join(path,fname)) as f:
+                        add_ubx(f)
+        
