371500e23a606977d3d2fc569f22d42bdb7c89e1
Theo Kleinendorst <tkleinen@gmail.com>
2017-09-10 16:15:28 +0200

    tasypie api extented

--- peil/peil/api.py
+++ peil/peil/api.py
@@ -3,13 +3,14 @@ Created on Nov 30, 2016
 
 @author: theo
 '''
-from django.contrib.auth.models import User
+
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from tastypie.authorization import DjangoAuthorization
 from tastypie import fields
 from tastypie.authentication import BasicAuthentication
 
 from .models import Device
+from peil.models import Sensor, LoraMessage
 
 class OpenGETAuthentication(BasicAuthentication):
     def is_authenticated(self, request, **kwargs):
@@ -25,6 +26,41 @@ class DeviceResource(ModelResource):
         authorization = DjangoAuthorization()
         filtering = {
             'devid': ALL,
+            'displayname': ALL,
             'serial': ALL,
+            'last_seen': ALL
+            }
+    
+class SensorResource(ModelResource): 
+    device = fields.ForeignKey(DeviceResource,'device')
+    
+    def dehydrate(self, bundle):
+        if bundle.obj:
+            last = bundle.obj.last_message()
+            if last:
+                bundle.data['last_message'] = last.time 
+                bundle.data['last_value'] = last.value()
+        return bundle
+    
+    class Meta:
+        queryset = Sensor.objects.all()
+        resource_name = 'sensor'
+        authentication = BasicAuthentication(realm='Acacia Water')
+        authorization = DjangoAuthorization()
+        filtering = {
+            'device': ALL_WITH_RELATIONS,
+            'ident': ALL
+            }
+    
+class MessageResource(ModelResource): 
+    sensor = fields.ForeignKey(SensorResource,'sensor')
+    class Meta:
+        queryset = LoraMessage.objects.all()
+        resource_name = 'message'
+        authentication = BasicAuthentication(realm='Acacia Water')
+        authorization = DjangoAuthorization()
+        filtering = {
+            'sensor': ALL_WITH_RELATIONS,
+            'time': ALL
             }
     
